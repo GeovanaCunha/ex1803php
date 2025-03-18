9eb4938e6502330ce9fefdfc6543ef486075177b
IsabelyFaria <bellyfaria04@gmail.com>
2025-03-18 11:31:49 -0300

    INDEX FEITO

---
+++ exercicio 1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>soma</title>
+</head>
+<body>
+	<fieldset>
+	<legend> numero </legend>
+	<form method="post" action="calcular.php">
+	Digite a temperatura em Celsius:
+	<input type= "text" name="conta1" /> <br/>
+	
+	
+	<input type="submit" name="somarValores" value="Mostrar o resultado">
+	
+	<br/>
+	</form>
+	</fieldset>
+</body>
+</html>
