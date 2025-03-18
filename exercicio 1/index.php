cea2c2a818d39a42f0df762bb3611bac3a45c625
IsabelyFaria <bellyfaria04@gmail.com>
2025-03-18 11:33:29 -0300

    index

---
+++ exercicio 1/index.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Nome</title>
+</head>
+<body>
+	<fieldset>
+	<legend> numero maior</legend>
+	<form method="post" action="calcular.php">
+	digite 1 numero:
+	<input type= "text" textnumber="n1" /> <br/>
+	<br/>
+    <form method="post" action="calcular.php">
+	digite 2 numero:
+	<input type= "text" textnumber="n2" /> <br/>
+	<br/>
+	</form>
+	</fieldset>
+</body>
+</html>
